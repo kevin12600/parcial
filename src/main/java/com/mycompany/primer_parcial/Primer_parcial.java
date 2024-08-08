45e27b52c2ae6cbf2f5abbe740cb4356c64134e1
kevin12600 <kevinbro57@gmail.com>
2024-08-07 20:58:44 -0600

    prueba1

---
+++ src/main/java/com/mycompany/primer_parcial/Primer_parcial.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.primer_parcial;
+
+/**
+ *
+ * @author DELL LATITUDE 5401
+ */
+public class Primer_parcial {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
